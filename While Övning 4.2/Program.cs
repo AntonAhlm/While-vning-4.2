878334139ee914f1a798fb5476355c1c570602bd
Anton <>
2023-02-17 09:20:26 +0100

    Add project files.

---
+++ While Övning 4.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Tal = 50;
+            while (Tal>=1)
+            {
+                Console.WriteLine(Tal);
+                Tal--;
+
+            }
+        }
+    }
+}
